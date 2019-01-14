6e846e7ff9f9041620181b468a1b4b34435a3bdf
vthang95 <tranvietthang95@gmail.com>
2019-01-14 12:38:32 +0700

    Normalize chars

--- lib/slugger.ex
+++ lib/slugger.ex
@@ -1,7 +1,7 @@
 defmodule Slugger do
-
   # Default char separating
   @separator_char Application.get_env(:slugger, :separator_char, ?-)
+  @separator_normalize "-"
 
   # File that contains the char replacements.
   @replacement_file Application.get_env(:slugger, :replacement_file, "lib/replacements.exs")
@@ -34,13 +34,18 @@ defmodule Slugger do
       "Trimming-and-Removing-inside"
 
   """
-  @spec slugify(text :: any, separator :: char) :: String.t
+  @spec slugify(text :: any, separator :: char) :: String.t()
   def slugify(text, separator \\ @separator_char) do
     text
     |> handle_possessives
     |> replace_special_chars
     |> remove_unwanted_chars(separator, ~r/([^A-Za-z0-9가-힣])+/)
+    |> String.normalize(:nfd)
+    |> String.replace(normalize_regex(@separator_normalize), "")
   end
+
+  defp normalize_regex(separator),
+    do: Regex.compile!("[^A-z0-9#{separator}\s]", "u")
 
   @doc """
   Return a string in form of a lowercase slug for a given string.
@@ -60,43 +65,52 @@ defmodule Slugger do
       "trimming-and-removing-inside"
 
   """
-  @spec slugify_downcase(text :: any, separator :: char) :: String.t
+  @spec slugify_downcase(text :: any, separator :: char) :: String.t()
   def slugify_downcase(text, separator \\ @separator_char) do
     text
     |> handle_possessives
     |> replace_special_chars
-    |> String.downcase
+    |> String.downcase()
     |> remove_unwanted_chars(separator, ~r/([^a-z0-9가-힣])+/)
+    |> String.normalize(:nfd)
+    |> String.replace(normalize_regex(@separator_normalize), "")
   end
 
-  @spec remove_unwanted_chars(text :: String.t, separator :: char, pattern :: Regex.t) :: String.t
+  @spec remove_unwanted_chars(text :: String.t(), separator :: char, pattern :: Regex.t()) ::
+          String.t()
   defp remove_unwanted_chars(text, separator, pattern) do
     sep_binary = to_string([separator])
+
     text
     |> String.replace(pattern, sep_binary)
     |> String.trim(sep_binary)
   end
 
-  @spec replace_special_chars(text :: any) :: String.t
+  @spec replace_special_chars(text :: any) :: String.t()
   defp replace_special_chars(text) do
     text |> to_charlist |> replace_chars |> to_string
   end
 
-  #-- Generated function `replace_chars` below ---
+  # -- Generated function `replace_chars` below ---
 
   # Generate replacement functions using pattern matching.
   @spec replace_chars(charlist) :: charlist
   {replacements, _} = Code.eval_file(@replacement_file)
 
-  replacements_by_search =  replacements
-  |> Enum.group_by(fn({search, _}) -> search end, fn({_, replace}) -> replace end)
-  |> Enum.into([])
+  replacements_by_search =
+    replacements
+    |> Enum.group_by(fn {search, _} -> search end, fn {_, replace} -> replace end)
+    |> Enum.into([])
 
   # Output a warning, if a replacement is duplicated.
   replacements_by_search
-  |> Enum.each(fn({search, values}) ->
+  |> Enum.each(fn {search, values} ->
     if length(values) > 1 do
-      IO.puts("Slugger warning: duplicate replacement from #{inspect <<search::utf8>>} to #{inspect values}")
+      IO.puts(
+        "Slugger warning: duplicate replacement from #{inspect(<<search::utf8>>)} to #{
+          inspect(values)
+        }"
+      )
     end
   end)
 
@@ -104,12 +118,12 @@ defmodule Slugger do
   for {search, replaces} <- replacements_by_search do
     if search != @separator_char do
       replace = hd(replaces)
-      defp replace_chars([unquote(search)|t]), do: unquote(replace) ++ replace_chars(t)
+      defp replace_chars([unquote(search) | t]), do: unquote(replace) ++ replace_chars(t)
     end
   end
 
   # A unmatched char will be kept.
-  defp replace_chars([h|t]), do: [h] ++ replace_chars(t)
+  defp replace_chars([h | t]), do: [h] ++ replace_chars(t)
 
   # String has come to an end, stop recursion here.
   defp replace_chars([]), do: []
@@ -129,8 +143,10 @@ defmodule Slugger do
   """
   def truncate_slug(slug, max_length, options \\ [])
   def truncate_slug(_slug, max_length, _options) when max_length < 1, do: ""
+
   def truncate_slug(slug, max_length, options) do
     options = Keyword.merge(@truncation_defaults, options)
+
     slug
     |> to_charlist
     |> truncate_charlist(max_length, {options[:hard], options[:separator]})
@@ -138,7 +154,7 @@ defmodule Slugger do
   end
 
   defp truncate_charlist(slug, max_length, _) when length(slug) <= max_length,
-  do: slug
+    do: slug
 
   defp truncate_charlist(slug, max_length, {true, _}) do
     slug |> Enum.take(max_length)
@@ -148,24 +164,23 @@ defmodule Slugger do
     if has_separator(slug, max_length, separator) do
       slug
       |> Enum.take(max_length + 1)
-      |> Enum.reverse
+      |> Enum.reverse()
       |> Enum.drop_while(&(&1 != separator))
       |> Enum.drop(1)
-      |> Enum.reverse
+      |> Enum.reverse()
     else
       slug |> Enum.take(max_length)
     end
   end
 
   defp has_separator(slug, range, separator) do
-      slug
-      |> Enum.take(range)
-      |> Enum.any?(&(&1 == separator))
+    slug
+    |> Enum.take(range)
+    |> Enum.any?(&(&1 == separator))
   end
 
   # Handle "Sheep's Milk" so it will be "sheeps-milk" instead of "sheep-s-milk"
   defp handle_possessives(text) do
     String.replace(text, ~r/['’]s/u, "s")
   end
-
 end
